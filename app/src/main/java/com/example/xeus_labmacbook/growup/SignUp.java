6cbbbb154f6da1ee65a7edef1497e2dc7a91a8bd
Piyapol Raengtip <bigblackboomz@gmail.com>
2017-01-29 00:30:06 +0700

    Edit color Button signup and login
    Edit Intent Signup page

--- app/src/main/java/com/example/xeus_labmacbook/growup/SignUp.java
+++ app/src/main/java/com/example/xeus_labmacbook/growup/SignUp.java
@@ -1,13 +1,15 @@
 package com.example.xeus_labmacbook.growup;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
-public class SignUp extends AppCompatActivity {
+public class SignUp extends AppCompatActivity implements View.OnClickListener {
+
+    private Button buttSignup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +18,17 @@ public class SignUp extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        buttSignup = (Button)findViewById(R.id.buttSignup);
+        buttSignup.setOnClickListener(this);
     }
 
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.buttSignup:
+                Intent access = new Intent(this, Dashboard.class);
+                startActivity(access);
+                break;
+        }
+    }
 }
