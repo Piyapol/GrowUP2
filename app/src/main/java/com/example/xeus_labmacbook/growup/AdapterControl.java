97f567773b824abc1298855220044b5b43e8f962
Piyapol Raengtip <bigblackboomz@gmail.com>
2017-02-21 10:25:25 +0700

    add Adaptercontrol

---
+++ app/src/main/java/com/example/xeus_labmacbook/growup/AdapterControl.java
@@ -0,0 +1,8 @@
+package com.example.xeus_labmacbook.growup;
+
+/**
+ * Created by DuckWalkZ on 21/2/2560.
+ */
+
+public class AdapterControl {
+}
--- app/src/main/java/com/example/xeus_labmacbook/growup/Humidity.java
+++ app/src/main/java/com/example/xeus_labmacbook/growup/Humidity.java
@@ -5,12 +5,17 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 /**
  * Created by xeus_labmacbook on 1/26/2017 AD.
  */
 
 public class Humidity extends Fragment {
+
+    private  ListView listView;
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
@@ -18,7 +23,20 @@ public class Humidity extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
-        View view = inflater.inflate(R.layout.humidity, container, false);
-        return view;
+        String[] names = {"Temperature", "Humidity", "Soil moisture"};
+
+        int[] icons = {R.drawable.thermometer, R.drawable.humidity, R.drawable.grass, R.drawable.light, R.drawable.levels};
+        ArrayList<Item> arrayList = new ArrayList<>();
+        for(int i = 0; i < names.length; i++) {
+            arrayList.add(new Item(icons[i], names[i]));
+        }
+
+        View rootView = inflater.inflate(R.layout.humidity, container, false);
+
+        listView = (ListView) rootView.findViewById(R.id.listView);
+        final MyAdapter myAdapter = new MyAdapter(getActivity().getBaseContext(), R.layout.item_control, arrayList);
+        listView.setAdapter(myAdapter);
+
+        return rootView;
     }
 }
