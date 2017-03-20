cf5cdf948ce9131a70d828afcbc55c2ffd3f87ad
Piyapol Raengtip <bigblackboomz@gmail.com>
2017-03-21 00:40:30 +0700

    add profile

--- app/src/main/java/com/example/xeus_labmacbook/growup/Home.java
+++ app/src/main/java/com/example/xeus_labmacbook/growup/Home.java
@@ -82,9 +82,9 @@ public class Home extends AppCompatActivity
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
 
         return super.onOptionsItemSelected(item);
     }
