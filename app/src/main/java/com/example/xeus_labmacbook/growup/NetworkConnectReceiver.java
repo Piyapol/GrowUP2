cba3309767da61f823f547dc2e3bf57d06924a09
Piyapol Raengtip <bigblackboomz@gmail.com>
2017-04-14 16:08:29 +0700

    fix check internet

--- app/src/main/java/com/example/xeus_labmacbook/growup/NetworkConnectReceiver.java
+++ app/src/main/java/com/example/xeus_labmacbook/growup/NetworkConnectReceiver.java
@@ -4,7 +4,6 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
 import android.widget.Toast;
 
 /**
@@ -18,14 +17,33 @@ public class NetworkConnectReceiver extends BroadcastReceiver {
             ConnectivityManager connectivityManager =
                     (ConnectivityManager)context.getSystemService(Context.CONNECTIVITY_SERVICE);
 
-            NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
-            String str = " ";
-            if(networkInfo != null && networkInfo.isConnected()){
-                str = "Network is connect";
-            }else{
-                str = "Network is disconnect";
+            //For 3G check
+            boolean is3g = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE)
+                    .isConnectedOrConnecting();
+            //For WiFi Check
+            boolean isWifi = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI)
+                    .isConnectedOrConnecting();
+
+            System.out.println(is3g + " net " + isWifi);
+
+            if (!is3g && !isWifi)
+            {
+                Toast.makeText(context,"Please make sure your Network Connection is ON ",Toast.LENGTH_LONG).show();
             }
-            Toast.makeText(context, str, Toast.LENGTH_LONG).show();
+            else
+            {
+                Toast.makeText(context," Your method what you want to do ",Toast.LENGTH_LONG).show();
+
+            }
+
+//            NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+//            String str = " ";
+//            if(networkInfo != null && networkInfo.isConnected()){
+//                str = "Network is connect";
+//            }else{
+//                str = "Network is disconnect";
+//            }
+//            Toast.makeText(context, str, Toast.LENGTH_LONG).show();
         }
     }
 }
